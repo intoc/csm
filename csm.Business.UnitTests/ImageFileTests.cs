7d8a0a5573280725308eef9bcfe580755aea5f61
intoc <intoc1@gmail.com>
2022-09-21 15:13:50 -0400

    Rename image file tests

--- csm.Business.UnitTests/ImageFileTests.cs
+++ csm.Business.UnitTests/ImageFileTests.cs
@@ -4,7 +4,7 @@
         [Theory]
         [InlineData(@"cover.jpg", @".\")]
         [InlineData(@"C:\images\cover.jpg", @"C:\images\")]
-        public void ImageFile_DirectoryTheory(string path, string expected) {
+        public void ImageFile_Directory_Theory(string path, string expected) {
             ImageFile file = new(path);
             Assert.Equal(expected, file.Directory);
         }
@@ -12,7 +12,7 @@
         [Theory]
         [InlineData(@"cover.jpg", @"cover.jpg")]
         [InlineData(@"C:\images\cover.jpg", @"cover.jpg")]
-        public void ImageFile_FileNameTheory(string path, string expected) {
+        public void ImageFile_FileName_Theory(string path, string expected) {
             ImageFile file = new(path);
             Assert.Equal(expected, file.FileName);
         }
@@ -21,7 +21,7 @@
         [InlineData(@"cover.jpg", @".jpg")]
         [InlineData(@"cover.infix.jpg", @".jpg")]
         [InlineData(@"cover", null)]
-        public void ImageFile_ExtensionTheory(string path, string expected) {
+        public void ImageFile_Extension_Theory(string path, string expected) {
             ImageFile file = new(path);
             Assert.Equal(expected, file.Extension);
         }
