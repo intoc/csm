2f1108fe4c33aaa8eacc04b9d99c09448aa22908
intoc <intoc1@gmail.com>
2022-09-21 23:03:37 -0400

    Only load settings and cmd line args once at start

--- csm.WinForms/Program.cs
+++ csm.WinForms/Program.cs
@@ -32,30 +32,27 @@ static class Program {
             // Search all arguments for a path, use the first one that shows up
             var path = args.FirstOrDefault(a => !a.StartsWith("-"));
 
-            // Load a settings file if path is provided
+            // Load a settings file
             var sFileParamAndValue = args.FirstOrDefault(a => a.ToLower().StartsWith("-sfile="));
             if (sFileParamAndValue != null) {
                 cs.LoadParamsFromFile(Param.GetValueFromCmdParamAndValue(sFileParamAndValue));
+            } else if (cs.GuiEnabled) {
+                cs.LoadParamsFromFile(DEFAULT_SETTINGS_FILE);
             }
 
+            // Any parameters from the command line should override the settings file
             cs.LoadParamsFromCommandLine(args);
 
             // Prompt for arguments graphically
             if (cs.GuiEnabled) {
-                // Load default settings file if none was supplied from the CLA
-                if (cs.SettingsFile == null && cs.LoadParamsFromFile(DEFAULT_SETTINGS_FILE)) {
-                    // Load parameters from command line arguments again to override the settings file
-                    cs.LoadParamsFromCommandLine(args);
+                if (path != null) {
+                    cs.Source = path;
                 }
 
                 CsmGui gui = new(cs);
                 gui.FormClosed += async (sender, args) => {
                     await Log.CloseAndFlushAsync();
                 };
-
-                if (path != null) {
-                    cs.Source = path;
-                }
 
                 // Show a GUI for parameter customization
                 Application.EnableVisualStyles();
